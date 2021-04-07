69fb41a97c738bbabde2b532534706576846a873
logicp <logicp@logicp.ca>
2021-04-06 20:47:12 -0400

    UT

---
+++ test/ut_kwitter/kwitter.test.cpp
@@ -0,0 +1,6 @@
+#include "kwitter.test.hpp"
+
+
+TEST(kwitterTests, DISABLED_Instantiate) {
+  ASSERT_NO_THROW(kwitter::Client{});
+}
---
+++ test/ut_kwitter/kwitter.test.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "gtest/gtest.h"
+
+#include "kwitter/bot/bot.hpp"
+
+const std::string TEST_PATH{kwitter::get_executable_cwd() + "../test/ut_kwitter/data/test_file.jpg"};
+
