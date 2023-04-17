9d9413f5e07c7b28e6899b2a34ceda92c11fbada
logicp <logicp@logicp.ca>
2023-04-16 20:51:49 -0400

    fix and cleanup

--- src/kwitter/bot/bot.hpp
+++ src/kwitter/bot/bot.hpp
@@ -4,12 +4,11 @@
 #include <nlp.hpp>
 
 namespace kwitter {
-
 static std::vector<File> GetDefaultFilesArg()
 {
   return std::vector<File>{};
 }
-
+//------------------------------------------------------------------
 static void SortPopularity(Tweets& tweets)
 {
   std::sort(tweets.begin(), tweets.end(), [](const Tweet& a, const Tweet& b)
@@ -19,32 +18,30 @@ static void SortPopularity(Tweets& tweets)
     return a_num > b_num;
   });
 };
-
+//------------------------------------------------------------------
 static void FilterMedia(Tweets& tweets)
 {
   tweets.erase(
     std::remove_if(tweets.begin(), tweets.end(), [](const Tweet& tweet) { return !(tweet.has_media()); }),
     tweets.end());
 }
-
+//------------------------------------------------------------------
 static void FilterIfMedia(Tweets& tweets)
 {
   for (const auto& tweet : tweets)
     if (tweet.has_media()) return FilterMedia(tweets);
 }
-
+//------------------------------------------------------------------
 static void FilterTodayOnly(Tweets& tweets)
 {
   tweets.erase(
     std::remove_if(tweets.begin(), tweets.end(), [](const Tweet& t)  { return (is_today(to_time_t(t.created_at))); }),
     tweets.end());
 }
-
+//------------------------------------------------------------------
+//------------------------------------------------------------------
 class Bot
-// :
-// public ConversationTracker
 {
-
 public:
 Bot(const std::string& username = "")
 : m_client(Client{
@@ -55,15 +52,7 @@ Bot(const std::string& username = "")
     throw std::invalid_argument{"Client was not able to authenticate"};
   }
 }
-
-
-/**
- * @brief
- *
- * @param   [in]  {Tweet}            Tweet
- * @param   [in]  {std::vector<File>} files
- * @returns [out] {bool}
- */
+//------------------------------------------------------------------
 template <typename T = File>
 bool PostTweet(Tweet tweet = Tweet{}, std::vector<T> files = GetDefaultFilesArg()) {
   try
@@ -79,34 +68,12 @@ bool PostTweet(Tweet tweet = Tweet{}, std::vector<T> files = GetDefaultFilesArg(
   }
   return false;
 }
-
-/**
- * @brief ReplyToTweet
- *
- * @param   [in]  {Tweet}      Tweet
- * @param   [in]  {std::string} message
- * @param   [in]  {bool}        remove_id
- * @returns [out] {bool}
- */
-bool ReplyToTweet(Tweet tweet, std::string message = "", bool remove_id = true)
-{
-  Tweet reply;
-
-  reply = Tweet{};
-  // reply.content = message;
-
-  // reply.replying_to_id = std::to_string(tweet.id);
-  // reply.content        = MakeMention(tweet) + reply.content;
-  // reply.visibility     = tweet.visibility;
-
-  return m_client.PostTweet(reply);
-}
-
+//------------------------------------------------------------------
 Tweet FetchTweet(TwitterStatusClient::TweetID id)
 {
   return m_client.FetchTweet(id);
 }
-
+//------------------------------------------------------------------
 std::vector<Tweet> FetchUserTweets(const std::string& user_id, uint8_t max = 10, bool get_threads = false)
 {
   auto tweets = m_client.FetchUserTweets(user_id);
@@ -114,27 +81,23 @@ std::vector<Tweet> FetchUserTweets(const std::string& user_id, uint8_t max = 10,
   {
     Tweets thread_tweets;
     for (const auto& tweet : tweets)
-    {
       if (!tweet.conversation_id.empty() && (tweet.author_id == tweet.in_reply_to_user_id))
-      {
         thread_tweets = m_client.FetchThread(tweet.conversation_id, tweet.author_id);
-      }
-    }
     tweets.insert(tweets.end(), std::make_move_iterator(thread_tweets.begin()), std::make_move_iterator(thread_tweets.end()));
   }
   return tweets;
 }
-
+//------------------------------------------------------------------
 std::vector<Tweet> FetchUserTweetsV1(const std::string& username, uint8_t max = 10)
 {
   return m_client.FetchUserTweetsV1(username);
 }
-
+//------------------------------------------------------------------
 std::string FetchTweetsByUserJSON(const std::string& username, uint8_t max = 10, bool get_threads = false)
 {
-  return Tweet::TweetsToJSON(m_client.FetchUserTweets(username, max, get_threads));
+  return Tweet::TweetsToJSON(FetchUserTweets(username, max, get_threads));
 }
-
+//------------------------------------------------------------------
 std::string FetchTopTweetByDateJSON(const std::string& username, uint8_t max = 1, bool prefer_media = false)
 {
   auto tweets = m_client.FetchUserTweetsV1(username);
@@ -142,7 +105,7 @@ std::string FetchTopTweetByDateJSON(const std::string& username, uint8_t max = 1
   SortPopularity(tweets);
   return Tweet::TweetsToJSON(Tweets{tweets.begin(), tweets.begin() + max});
 }
-
+//------------------------------------------------------------------
 std::string FetchTweetsByTopicJSON(const std::string& topic, bool prefer_media = false, uint8_t max = 50)
 {
   auto tweets = m_client.FetchTweets(topic, max, prefer_media);
@@ -156,13 +119,12 @@ std::string FetchTweetsByTopicJSON(const std::string& topic, bool prefer_media =
     Tweets{tweets.begin(), tweets.begin() + max} : tweets;
   return Tweet::TweetsToJSON(final_tweets);
 }
-
-
+//------------------------------------------------------------------
 const bool SetUser(const std::string& username)
 {
   return m_client.SetUser(username);
 }
-
+//------------------------------------------------------------------
 const std::string GetLastError() const
 {
   return m_last_error;
